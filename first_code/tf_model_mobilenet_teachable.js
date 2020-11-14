65f1413ac255f2cecd70d42fc067a392976a2c61
mobile44 <42382113+mobile44@users.noreply.github.com>
2020-11-14 22:29:41 +0800

    Update tf_model_mobilenet_teachable.js

--- first_code/tf_model_mobilenet_teachable.js
+++ first_code/tf_model_mobilenet_teachable.js
@@ -1,6 +1,8 @@
+const webcamElement = document.getElementById('webcam');
+
 let net;
 
-async function app() {
+async function apptest() {
   console.log('Loading mobilenet..');
 
   // Load the model.
@@ -13,4 +15,30 @@ async function app() {
   console.log(result);
 }
 
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Successfully loaded model');
+  
+  // Create an object from Tensorflow.js data API which could capture image 
+  // from the web camera as Tensor.
+  const webcam = await tf.data.webcam(webcamElement);
+  while (true) {
+    const img = await webcam.capture();
+    const result = await net.classify(img);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+    // Dispose the tensor to release the memory.
+    img.dispose();
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
+}
 app();
