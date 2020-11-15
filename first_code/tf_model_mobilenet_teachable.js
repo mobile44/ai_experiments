fa6e2a5c060454aab67ad665f175fe853a8d091a
mobile44 <42382113+mobile44@users.noreply.github.com>
2020-11-15 09:19:17 +0800

    Update tf_model_mobilenet_teachable.js

--- first_code/tf_model_mobilenet_teachable.js
+++ first_code/tf_model_mobilenet_teachable.js
@@ -44,7 +44,7 @@ async function app() {
 async function listelements() {
   var videoTags = document.getElementsByTagName('video')
   for( var i = 0; i < videoTags.length; i++ ){
-      console.log( videoTags.item(i).currentSrc )
+      console.log( videoTags.item(i).src )
   }
 }
 
