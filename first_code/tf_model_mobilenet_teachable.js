8569ab64f6351cc16cf2370394a33d2199928cd4
mobile44 <42382113+mobile44@users.noreply.github.com>
2020-11-15 14:19:04 +0800

    Update tf_model_mobilenet_teachable.js

--- first_code/tf_model_mobilenet_teachable.js
+++ first_code/tf_model_mobilenet_teachable.js
@@ -1,47 +1,83 @@
-const webcamElement = document.getElementById('webcam');
-var webcam;
-var img;
-var result;
-let net;
+<script>
+    /* JS comes here */
+    (function() {
 
-async function apptest() {
-  console.log('Loading mobilenet..');
+        var width = 320; // We will scale the photo width to this
+        var height = 0; // This will be computed based on the input stream
 
-  // Load the model.
-  net = await mobilenet.load();
-  console.log('Successfully loaded model');
+        var streaming = false;
 
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
-}
+        var video = null;
+        var canvas = null;
+        var photo = null;
+        var startbutton = null;
 
-async function app() {
-  console.log('Loading mobilenet..');
-  
-  // Load the model.
-  net = await mobilenet.load();
-  console.log('Successfully loaded model');
-  
-  // Create an object from Tensorflow.js data API which could capture image 
-  // from the web camera as Tensor.
-  webcam = await tf.data.webcam(webcamElement);
-  while (true) {
-    mg = await webcam.capture();
-    result = await net.classify(img);
+        function startup() {
+            video = document.getElementById('video');
+            canvas = document.getElementById('canvas');
+            photo = document.getElementById('photo');
+            startbutton = document.getElementById('startbutton');
 
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
-    // Dispose the tensor to release the memory.
-    img.dispose();
+            navigator.mediaDevices.getUserMedia({
+                    video: true,
+                    audio: false
+                })
+                .then(function(stream) {
+                    video.srcObject = stream;
+                    video.play();
+                })
+                .catch(function(err) {
+                    console.log("An error occurred: " + err);
+                });
 
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
-    await tf.nextFrame();
-  }
-}
+            video.addEventListener('canplay', function(ev) {
+                if (!streaming) {
+                    height = video.videoHeight / (video.videoWidth / width);
 
-app();
+                    if (isNaN(height)) {
+                        height = width / (4 / 3);
+                    }
+
+                    video.setAttribute('width', width);
+                    video.setAttribute('height', height);
+                    canvas.setAttribute('width', width);
+                    canvas.setAttribute('height', height);
+                    streaming = true;
+                }
+            }, false);
+
+            startbutton.addEventListener('click', function(ev) {
+                takepicture();
+                ev.preventDefault();
+            }, false);
+
+            clearphoto();
+        }
+
+
+        function clearphoto() {
+            var context = canvas.getContext('2d');
+            context.fillStyle = "#AAA";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            var data = canvas.toDataURL('image/png');
+            photo.setAttribute('src', data);
+        }
+
+        function takepicture() {
+            var context = canvas.getContext('2d');
+            if (width && height) {
+                canvas.width = width;
+                canvas.height = height;
+                context.drawImage(video, 0, 0, width, height);
+
+                var data = canvas.toDataURL('image/png');
+                photo.setAttribute('src', data);
+            } else {
+                clearphoto();
+            }
+        }
+
+        window.addEventListener('load', startup, false);
+    })();
+    </script>
