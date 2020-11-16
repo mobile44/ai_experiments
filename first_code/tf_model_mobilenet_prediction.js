9e84242e830fe622f23c9d4719a9d258c24cbc3e
mobile44 <42382113+mobile44@users.noreply.github.com>
2020-11-16 11:04:59 +0800

    Update tf_model_mobilenet_prediction.js

--- first_code/tf_model_mobilenet_prediction.js
+++ first_code/tf_model_mobilenet_prediction.js
@@ -2,17 +2,19 @@ var width = 320; // We will scale the photo width to this
 var height = 0; // This will be computed based on the input stream
 var streaming = false;
 var video = null;
+/*
 var canvas = null;
 var photo = null;
 var startbutton = null;
+*/
 
 const classifier = knnClassifier.create();
 
 function startup() {
     video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('startbutton');
+    //canvas = document.getElementById('canvas');
+    //photo = document.getElementById('photo');
+    //startbutton = document.getElementById('startbutton');
 
     navigator.mediaDevices.getUserMedia({
         video: true,
@@ -36,21 +38,22 @@ function startup() {
 
             video.setAttribute('width', width);
             video.setAttribute('height', height);
-            canvas.setAttribute('width', width);
-            canvas.setAttribute('height', height);
+            //canvas.setAttribute('width', width);
+            //canvas.setAttribute('height', height);
             streaming = true;
          }
     }, false);
-
+    /*
     startbutton.addEventListener('click', function(ev) {
         takepicture();
         ev.preventDefault();
     }, false);
 
     clearphoto();
+    */
 }
 
-
+/*
 function clearphoto() {
     var context = canvas.getContext('2d');
     context.fillStyle = "#AAA";
@@ -59,7 +62,8 @@ function clearphoto() {
     var data = canvas.toDataURL('image/png');
     photo.setAttribute('src', data);
 }
-
+*/
+/*
 function takepicture() {
     var context = canvas.getContext('2d');
     if (width && height) {
@@ -72,6 +76,7 @@ function takepicture() {
     } else {
         clearphoto();
     }
+*/
 }
 
 async function predictapp() {
