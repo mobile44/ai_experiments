fac05fd944b9e1ed9965946242d3a29f7cd90c4d
mobile44 <42382113+mobile44@users.noreply.github.com>
2020-11-19 15:02:18 +0800

    Update sort_0_9.js

--- lessons/speak_out_numbers/sort_0_9.js
+++ lessons/speak_out_numbers/sort_0_9.js
@@ -4,6 +4,18 @@ const wordList = ["zero","one","two","three","four","five","six","seven","eight"
 const numList = ["zero","one","two","three","four","five","six","seven","eight","nine"];
 const gameList = [...numList];
 let modelLoaded = false;
+
+const data = [
+  { index: 0, value: 50 },
+  { index: 1, value: 100 },
+  { index: 2, value: 150 },
+];
+
+// Get a surface
+const surface = tfvis.visor().surface({ name: 'Barchart', tab: 'Charts' });
+
+// Render a barchart on that surface
+tfvis.render.barchart(surface, data, {});
         
 document.addEventListener('DOMContentLoaded', () => {
     const wrapperElement = document.getElementById('sp-cmd-wrapper');
